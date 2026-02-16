3d9aaac6535a1a37e8cf3abc1adcefb49b728b45
Mark Richardson <mrichardson@rws.com>
2026-02-16 10:29:15 +0000

    stash has working local hardcoded data in grid

---
+++ Controllers/GetDataController.cs
@@ -0,0 +1,30 @@
+﻿using BlackBoxCmdb;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class GetDataController(DataService dataService) : ControllerBase
+{
+    [HttpGet("{type}")]
+    public IActionResult GetData(string type)
+    {
+        return type?.ToLower() switch
+        {
+            "server" => Ok(GetServerData()),
+            "volume" => Ok(GetVolumeData()),
+            _ => NotFound()
+        };
+    }
+
+    private object GetServerData()
+    {
+        var servers = JsonSerializer.Serialize(dataService.Data, new JsonSerializerOptions { WriteIndented = true });
+        return servers;
+    }
+
+    private object GetVolumeData()
+        => new { Drive = "C:", FreeSpace = "100GB" };
+}
