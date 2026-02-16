0a4acf30801b76444c9fd0a8bb94bdd79c37d4d0
Mark Richardson <mrichardson@rws.com>
2026-02-16 11:39:47 +0000

    load json from local data endpoint

--- Controllers/GetDataController.cs
+++ Controllers/GetDataController.cs
@@ -15,6 +15,7 @@ public class GetDataController(DataService dataService) : ControllerBase
         {
             "server" => Ok(GetServerData()),
             "volume" => Ok(GetVolumeData()),
+            "test" => Ok(GetTestData()),
             _ => NotFound()
         };
     }
@@ -24,7 +25,29 @@ public class GetDataController(DataService dataService) : ControllerBase
         var servers = JsonSerializer.Serialize(dataService.Data, new JsonSerializerOptions { WriteIndented = true });
         return servers;
     }
+    private object GetTestData()
+    {
+        var testdata = new List<TestData>();
+        testdata.Add(new TestData
+        {
+            make = "SDL",
+            price = 202
+        });
+        return testdata;
+    }
 
     private object GetVolumeData()
         => new { Drive = "C:", FreeSpace = "100GB" };
+
+
+
+
+
+    public class TestData
+    {
+        public string make { get; set; }
+        public string model { get; set; }
+        public int price { get; set; }
+    }
+
 }
